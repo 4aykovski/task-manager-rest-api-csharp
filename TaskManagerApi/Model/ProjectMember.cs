92948d17ec06edabc45c653b82718aacc15b81f1
4aykovski <kan89kan@gmail.com>
2024-03-20 19:03:47 +0300

    feat: projectMembers model

--- TaskManagerApi/Model/ProjectMember.cs
+++ TaskManagerApi/Model/ProjectMember.cs
@@ -2,5 +2,8 @@
 
 public class ProjectMember
 {
-    // TODO: implement
+    public Guid UserId { get; set; }
+    public int ProjectId { get; set; }
+    public User User { get; set; }
+    public Project Project { get; set; }
 }
